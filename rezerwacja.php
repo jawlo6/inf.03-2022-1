dd0f0c22b63e0283a1aad24b4b99ac5e8e31d22e
jawlo6 <januszwlodarz@gmail.com>
2022-06-14 14:58:49 +0200

    koncowy

--- rezerwacja.php
+++ rezerwacja.php
@@ -1,27 +1,22 @@
 <?php
-    $servername = "localhost";
-    $username   = "root";
-    $password   = "";
-    $dbname     = "baza";
-
     $data       = $_POST["date"];
     $liczba     = $_POST["liczba"];
     $telefon    = $_POST["telefon"];
         
+    $servername = "localhost";
+    $username   = "root";
+    $password   = "";
+    $dbname     = "baza";
+    
     $conn=mysqli_connect($servername,$username,$password,$dbname);
     if( !$conn ){
         die( "Nieudane połączenie".mysqli_connect_error());
     }
-    
-        
-    echo "Dodano rezerwację do bazy";
 
-    $sql="insert into rezerwacje( data_rez,liczba_osob,nr_stolika, telefon) values($data, $liczba, 1, $telefon)";
-    echo @sql;
-        
-    if(mysqli_query($conn,$sql)){
+    $sql="insert into rezerwacje( data_rez,liczba_osob,telefon)                                         values(\"$data\", $liczba, $telefon)";
+   
+    if( mysqli_query($conn,$sql) ){
         echo "Dodano rezerwację do bazy";
-        echo "<br>$data $liczba $telefon";
     }else{
         echo "error: ". mysqli_error($conn);
     }
